ad00c4743dbafb97dca6e7e8f4d9b2d4f43fe11b
Fausto Calviño <faustocalvino@outlook.com>
2024-06-17 21:05:00 +0200

    configured api env variable

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const baseURL = axios.create({
-   baseURL: "http://localhost:3000/api/",
+   baseURL: import.meta.env.VITE_API_BASE_URL,
    headers: { "x-token": localStorage.getItem("token") },
 });
